ed49c9afa350d31ada79e915f6456729df67267d
Saleh Mohammad Saleh Alhumaydani <S.Alhumaydani@qu.edu.sa>
2023-03-09 11:12:59 +0300

    test

---
+++ pages/footer-ar.php
@@ -0,0 +1,82 @@
+  <!-- start footer -->
+  <footer class="py-5 ">
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-3">
+              <div class="list-group">
+                <span class="title-list">معلومات تخص</span>
+                <a href="" class="list-group-item list-group-item-action">الطلاب</a>
+                <a href="" class="list-group-item list-group-item-action">اعضاء هيئة التدريس</a>
+                <a href="" class="list-group-item list-group-item-action">الموظفون</a>
+                <a href="" class="list-group-item list-group-item-action">الخريجين</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <ul class="list-group">
+                <span class="title-list">الخدمات الإلكترونية</span>
+                <a href="" class="list-group-item list-group-item-action">MyQU</a>
+                <a href="" class="list-group-item list-group-item-action">بوابة الزوار</a>
+                <a href="" class="list-group-item list-group-item-action">البريد الإلكتروني</a>
+                <a href="" class="list-group-item list-group-item-action">BlackBoard</a>
+                <a href="" class="list-group-item list-group-item-action">المكتبة</a>
+                <a href="" class="list-group-item list-group-item-action">نظام إنجاز</a>
+                <a href="" class="list-group-item list-group-item-action">دليل الخدمات الإلكترونية</a>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <ul class="list-group">
+                <span class="title-list">أخرى</span>
+                <a href="" class="list-group-item list-group-item-action">التقويم</a>
+                <a href="" class="list-group-item list-group-item-action">منصة الفعاليات</a>
+                <a href="" class="list-group-item list-group-item-action">البيانات المفتوحة</a>
+                <a href="" class="list-group-item list-group-item-action">منصة التطوع</a>
+                <a href="" class="list-group-item list-group-item-action">منصة نماء</a>
+                <a href="" class="list-group-item list-group-item-action">الخرائط و الاتجاهات</a>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <ul class="list-group">
+                <span class="title-list">تواصل معنا</span>
+                <a href="" class="list-group-item list-group-item-action">دليل التواصل</a>
+                <a href="" class="list-group-item list-group-item-action">خدمة المستفيدين</a>
+                <a href="" class="list-group-item list-group-item-action">ساعد التقني</a>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="logo">
+            <img class="img-fluid" src="images/white_logo.png" alt="">
+          </div>
+          <div class="adress">
+            <p class="country">المملكة العربية السعودية</p>
+            <p class="city">القصيم - بريدة</p>
+            <p class="pox">ص.ب: 960 - الرمز البريدي:61421</p>
+          </div>
+          <div class="social-media">
+            <a class="btn main-btn rounded-pill"><i class="fa-brands fa-twitter"></i></a>
+            <a class="btn main-btn rounded-pill"><i class="fa-brands fa-instagram"></i></a>
+            <a class="btn main-btn rounded-pill"><i class="fa-brands fa-snapchat"></i></a>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row text-center">
+        <div class="copy-right col-sm-4"><span>جميع الحقوق محفوظة لجامعة القصيم &copy 2023</span></div>
+        <div class="conditaion col-sm-4">
+          <a href="" class="main-btn">شروط الاستخدام</a>
+          <span class="dot"><i class="fa-solid fa-circle"></i></span>
+          <a href="" class="main-btn">سياسة الخصوصية</a>
+        </div>
+        <div class="develop col-sm-4">تطوير عمادة تقنية المعلومات</div>
+      </div>
+    </div>
+  </footer>
+  <!-- end footer -->
+  <script src="js/style.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/all.min.js"></script>
+</body>
+</html>
