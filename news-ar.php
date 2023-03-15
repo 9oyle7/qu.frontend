c84bf915a29cda4d6fc5bc7ed3e328d225b1a04f
saleh <9oyle7@gmail.com>
2023-03-15 08:51:32 +0300

    done from news page

--- news-ar.php
+++ news-ar.php
@@ -18,22 +18,42 @@
             </a>
         </div>
     </div>
-    <div class="row other-news text-center align-items-center">
-        <div class="col-md-12">
-        <div class="content-news">
-            <a href="" class="box-news"> 
-                <h3 class="title">الجامعة توفر مقرا لأخذ لقاح كوفيد 19 بالتعاون مع وزارة الصحة</h3>
-                <span class="date">19 فبراير 2023</span>
-                <p class="details d-none d-md-block">برعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيان</p>
-            </a>  
-        </div>  
-        <div class="content-news">
-            <a href="" class="box-news"> 
-                <h3 class="title">الجامعة توفر مقرا لأخذ لقاح كوفيد 19 بالتعاون مع وزارة الصحة</h3>
-                <span class="date">19 فبراير 2023</span>
-                <p class="details d-none d-md-block">برعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيان</p>
-            </a>  
-        </div>    
+    <div class="row other-news">
+        <div class="col-md-12 d-flex align-items-center mb-3">
+            <div class="img-news d-none d-md-block">
+              <img src="images/2023-02-19-06-51-21_1675168650_3212.jpeg" class="img-fluid" alt="">
+            </div>
+            <div class="content-news">
+                <a href="" class="box-news"> 
+                    <h3 class="title">الجامعة توفر مقرا لأخذ لقاح كوفيد 19 بالتعاون مع وزارة الصحة</h3>
+                    <p class="details d-none d-md-block">برعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيان</p>
+                    <span class="date">19 فبراير 2023</span>
+                </a>  
+            </div>      
+        </div>
+        <div class="col-md-12 d-flex align-items-center mb-3">
+            <div class="img-news d-none d-md-block">
+              <img src="images/2023-02-19-06-51-21_1675168650_3212.jpeg" class="img-fluid" alt="">
+            </div>
+            <div class="content-news">
+                <a href="" class="box-news"> 
+                    <h3 class="title">الجامعة توفر مقرا لأخذ لقاح كوفيد 19 بالتعاون مع وزارة الصحة</h3>
+                    <p class="details d-none d-md-block">برعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيانبرعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيانبرعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيانبرعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيانبرعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيانبرعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيان</p>
+                    <span class="date">19 فبراير 2023</span>
+                </a>  
+            </div>      
+        </div>
+        <div class="col-md-12 d-flex align-items-center mb-3">
+            <div class="img-news d-none d-md-block">
+              <img src="images/2023-02-19-06-54-39_1673802125_8990.jpeg" class="img-fluid" alt="">
+            </div>
+            <div class="content-news">
+                <a href="" class="box-news"> 
+                    <h3 class="title">الجامعة توفر مقرا لأخذ لقاح كوفيد 19 بالتعاون مع وزارة الصحة</h3>
+                    <p class="details d-none d-md-block">برعاية معالي وزير التعليم رئيس مجلس إدارة الاتحاد الرياضي للجامعات السعودية الأستاذ يوسف بن عبدالله البنيان</p>
+                    <span class="date">19 فبراير 2023</span>
+                </a>  
+            </div>      
         </div>
     </div>
     <!-- start pagination -->
@@ -46,6 +66,7 @@
             </li>
             <li class="page-item"><a class="page-link active" href="#">1</a></li>
             <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">. . .</a></li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
             <li class="page-item">
             <a class="page-link" href="#" aria-label="Next">
